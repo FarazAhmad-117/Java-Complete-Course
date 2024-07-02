65f799d31f971f67b6ea10202565b53dfa704a0d
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-02 09:47:36 +0500

    dsfs

--- Lecture 1- Inputs and Outputs/Input.java
+++ Lecture 1- Inputs and Outputs/Input.java
@@ -9,10 +9,22 @@ public class Input {
         // String name = sc.next();
         // System.out.println("Hello "+name);
         // sc.close();
+
+        //Done
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your name again:");
+        // String name = sc.nextLine();
+        // System.out.println("Hello "+name);
+        // sc.close();
+
+        // A Challenge
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name again:");
-        String name = sc.nextLine();
-        System.out.println("Hello "+name);
+        System.out.print("Enter a number");
+        int a = sc.nextInt();
+        System.out.print("Enter another number");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.println(String.format("The sum is ==>> %d", sum));
         sc.close();
     }
 }
