08ca926d4fed9d6a1740de5c93c428211a7cf274
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-05 23:42:52 +0500

    do whle done

---
+++ Lecture3 - Loops/DoWhile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int number = sc.nextInt();
+        sc.close();
+        do{
+            System.out.println("Number is "+number);
+            number--;
+        }while(number > 0);
+    }
+}
