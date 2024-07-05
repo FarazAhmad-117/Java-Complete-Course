1d412292cb3d2dbaaf1d5d302fb457af91690a68
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-05 23:12:56 +0500

    even odd with if else

--- Lecture2- Conditional Statements/IFELSE.java
+++ Lecture2- Conditional Statements/IFELSE.java
@@ -3,13 +3,22 @@ import java.util.*;
 
 public abstract class IFELSE {
     public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter your age: ");
+        // int age = sc.nextInt();
+        // if(age < 18){
+        //     System.out.println("I am sorry you are underage! Come again when you are adult!");
+        // }else{
+        //     System.out.println("Wellcome to the club!");
+        // }
+        // sc.close();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your age: ");
-        int age = sc.nextInt();
-        if(age < 18){
-            System.out.println("I am sorry you are underage! Come again when you are adult!");
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if(n%2 == 0){
+            System.out.println("Even");
         }else{
-            System.out.println("Wellcome to the club!");
+            System.out.println("Odd");
         }
         sc.close();
     }
