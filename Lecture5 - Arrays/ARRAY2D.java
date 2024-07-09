f331ec038d65cdc36483cfbd6d1b12bbd9cdcaa6
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-10 02:11:54 +0500

    2d arrays introduced

---
+++ Lecture5 - Arrays/ARRAY2D.java
@@ -0,0 +1,16 @@
+
+public class ARRAY2D {
+    public static void main(String[] argv){
+        // int marks[] = {1,2,3,4,5};
+        // int marks2[][] = {{1,2},{2,3,4},{5,6,7,8}};
+        // for(int i=0;i<marks2.length;i++){
+        //     for(int j=0;j<marks2[i].length;j++){
+        //         System.out.println(marks2[i][j]);
+        //     }
+        // }
+
+
+        
+
+    }
+}
