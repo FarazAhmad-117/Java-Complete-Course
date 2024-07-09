268166db38662d349862615b3e57a82849ba1a1d
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-10 02:09:42 +0500

    arrays introduced

---
+++ Lecture5 - Arrays/ARRAY.java
@@ -0,0 +1,18 @@
+
+public class ARRAY {
+    public static void main(String[] args) {
+        // String[] arr = {"Ali","Ahmad"};
+        // for(int i=0;i<arr.length ; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+        // int[] table = new int[10];
+        // for(int i=0;i<10;i++){
+        //     table[i] = (i+1) *5;
+        // }
+        // for(int i=0;i<10;i++){
+        //     System.out.println(table[i]);
+        // }
+
+    }
+}
