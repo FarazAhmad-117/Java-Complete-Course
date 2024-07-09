6109db16421489cf192490b3baccfb8e2441c6f5
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-10 02:33:16 +0500

    string is immutable

---
+++ Lecture6 - Strings/STRINGS.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class STRINGS {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a string:");
+        // String str = sc.nextLine();
+        // sc.close();
+        String str= "Altaf";
+        System.out.println(str);
+
+        System.out.println(str.length());
+
+        String str1 = "Ali";
+        String str2 = "Ali";
+        if(str1 == str2){
+            System.out.println("They are equal");
+        }else{
+            System.out.println("They are not Equal");
+        }
+
+        if(new String("Ali") == new String("Ali")){
+            System.out.println("They are equal");
+        }else{
+            System.out.println("They are not Equal");
+        }
+
+        int num = Integer.valueOf("123");
+        System.out.println(num);
+
+    }
+}
