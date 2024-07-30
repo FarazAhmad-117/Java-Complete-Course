13d1d9f5914636288608d84c384f8c1178513862
Faraz Ahmad <farazahmad31048@gmail.com>
2024-07-30 10:41:09 +0500

    done

--- Lecture10 - OOPs/OOPs.java
+++ Lecture10 - OOPs/OOPs.java
@@ -7,7 +7,22 @@ public class OOPs {
         Pen p= new Pen();
         p.color = "red";
         p.type = "ballpen";
-        p.display();
+        // p.display();
+
+        // Human h1 = new Human("Faraz Ahmad", 18);
+        // h1.displayInfo();
+        // // Human h2 = h1; // Same info not created another string
+        // // h2.displayInfo();
+        // Human h2 = new Human(h1); // Created anohter string in memory
+        // h1.name = "Altaf";
+        // h2.displayInfo();
+
+        // Learning Polymorphism
+        ShapeConstructor sc = new ShapeConstructor();
+        sc.printShape();
+        sc.printShape(23);
+        sc.printShape("Hello");
+
     }
 
     /*
@@ -28,3 +43,85 @@ class Pen{
     }
 
 }
+
+
+// Constructors
+
+class Human {
+    String name;
+    int age;
+
+    Human(String str,int a){
+        this.name = str;
+        this.age = a;
+    }
+
+    Human(Human h){
+        this.name = h.name;
+        this.age = h.age;
+    }
+
+    void getAge(int a){
+        this.age = a;
+    }
+
+    void getName(String str){
+        this.name = str;
+    }
+
+    void displayInfo(){
+        System.out.println("Name = "+this.name+" Age = "+String.valueOf(this.age));
+    }
+
+}
+
+
+
+// Polymorphisim
+
+// Functon Overloading
+
+class ShapeConstructor{
+
+    public void printShape(){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    public void printShape(String str){
+        for(int i=0;i<9;i++){
+            for(int j=0;j<4;j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+
+    public void printShape(int a){
+        for(int i=0;i<4;i++){
+            for(int j=0;j<9;j++){ 
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    
+}
+
+
+
+
+
+
+
+
+
+
+
